fcf627fd7452d9525b335575c18bacf537b41650
Singh-Aviraj <148427283+Singh-Aviraj@users.noreply.github.com>
2024-11-26 22:04:55 +0530

    Farmer Decision Support System built in Reactjs and Spring Boot
    Farmer Decision Support System

--- blogging/src/main/java/com/example/blogging/services/UserServiceImpl.java
+++ blogging/src/main/java/com/example/blogging/services/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.blogging.services;
 
+import com.example.blogging.entity.User;
 import com.example.blogging.payloads.UserDTO;
 
 import java.util.List;
@@ -20,6 +21,24 @@ public class UserServiceImpl {
         return null;
     }
     public void deleteUser(Integer userid) {
+    }
+    private User DTOtoUser(UserDTO userDTO) {
+        User user = new User();
+        user.setId(userDTO.getId());
+        user.setName(userDTO.getName());
+        user.setEmail(userDTO.getEmail());
+        user.setPassword(userDTO.getPassword());
+        user.setAbout(userDTO.getAbout());
+        return user;
+    }
+    private UserDTO UserToDTO(User user) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(user.getId());
+        userDTO.setName(user.getName());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setPassword(user.getPassword());
+        userDTO.setAbout(user.getAbout());
+        return userDTO;
     }
 
 }
